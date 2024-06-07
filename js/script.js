0990c60f68b6b82aedb2cc3566b328c4bc138987
Tengen777 <theo30m@gmail.com>
2024-06-07 19:48:03 +0200

    Update script.js
    prompt in plaats van alert

--- js/script.js
+++ js/script.js
@@ -5,3 +5,13 @@ alert("Hello World") // oefening 1
 let a = 10; 
 a = a+5;
 console.log(a);
+
+// oefening 3
+let leeftijd = prompt("wat is je leeftijd");
+
+    if (leeftijd > 18){
+        console.log("welkom");
+
+    } else {
+        console.log("No entry");
+    }
