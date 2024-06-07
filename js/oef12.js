b3c68e5621dc0ad85b20d8a4b3b2539bdad777b9
Tengen777 <theo30m@gmail.com>
2024-06-07 21:23:00 +0200

    Create oef12.js
    array met maanden aanmaken

---
+++ js/oef12.js
@@ -0,0 +1,4 @@
+"user strict"
+const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+const today = new Date();
