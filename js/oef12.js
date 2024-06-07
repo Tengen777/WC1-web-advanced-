6873595b9eebae7390cfa031ff94acf7f7221333
Tengen777 <theo30m@gmail.com>
2024-06-07 21:26:57 +0200

    Update oef12.js
    de constanten aanmaken: day month en year (+ get)

--- js/oef12.js
+++ js/oef12.js
@@ -1,6 +1,11 @@
 "user strict"
 const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 
+const day = today.getDate();
+const month = months[today.getMonth()];
+const year = today.getFullYear();
+
+
 const today = new Date();
 
 console.log(`${day} ${month} ${year}`);
