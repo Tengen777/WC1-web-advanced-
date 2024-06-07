637f28378c333c6c413674737ebd175bb0a73b83
Tengen777 <theo30m@gmail.com>
2024-06-07 21:02:47 +0200

    Update oef8.js
    return erbij

--- js/oef8.js
+++ js/oef8.js
@@ -2,14 +2,14 @@
 
 function capitalizeEersteLetter(woord){
     woord = woord.trim(); 
-    let woord = woord.split(" ");
+    let woord = woord.split(" "); // woord wordt een array
     
     for (let part in woord){
         
      woord[part] = (woord[part].slice(0).toUppercase() + woord[part].slice(1));
    }
    
-    console.log(capitalizeEersteLetter);
+    return woord.join(" ");
 }
 
 let name1 = 'mike derycke';
