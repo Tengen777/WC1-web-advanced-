9d60e6814b7433cbaac497f12605373224e149e6
Tengen777 <theo30m@gmail.com>
2024-06-07 21:23:59 +0200

    Update oef12.js
    console:  met nieuwe datum

--- js/oef12.js
+++ js/oef12.js
@@ -2,3 +2,5 @@
 const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 
 const today = new Date();
+
+console.log(`${day} ${month} ${year}`);
