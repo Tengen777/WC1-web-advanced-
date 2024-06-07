a9cc514733a1258ed240e84aa0a761e0154d5a34
Tengen777 <theo30m@gmail.com>
2024-06-07 23:21:18 +0200

    Update oef10.js

--- js/oef10.js
+++ js/oef10.js
@@ -1,13 +1,15 @@
+"user srtict"
 function vindLandMetLangsteNaam(Landen){
     
     let longest = ""; 
     for (longest in Landen){
-
-        
+        if (Landen.lenght > longest.length){
+            longest = landen; 
+        }
     }
 
 
-
-
+    return longest;
 
 }
+
